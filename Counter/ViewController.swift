a284a5a766daba61783664b6940d46166ff464d3
talkingwoman <aisshesoboleva@yandex.ru>
2026-02-14 02:53:26 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by VIctoria Soboleva on 14.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
